bb94d13f67c24304abc0fac57fd46c4c8cf9aac4
Tai <taiei.t03@gmail.com>
2022-11-14 19:59:07 +0900

    shorten index.js

--- index.js
+++ index.js
@@ -8,53 +8,10 @@ function castDiceFunction() {
   const randomNumber1 = Math.floor(Math.random() * 6) + 1;
   const randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-  switch (randomNumber1) {
-    case 1:
-      img1.setAttribute('src', './images/dice1.png');
-      break;
-    case 2:
-      img1.setAttribute('src', './images/dice2.png');
-      break;
-    case 3:
-      img1.setAttribute('src', './images/dice3.png');
-      break;
-    case 4:
-      img1.setAttribute('src', './images/dice4.png');
-      break;
-    case 5:
-      img1.setAttribute('src', './images/dice5.png');
-      break;
-    case 6:
-      img1.setAttribute('src', './images/dice6.png');
-      break;
-  }
-
-  switch (randomNumber2) {
-    case 1:
-      document;
-      img2.setAttribute('src', './images/dice1.png');
-      break;
-    case 2:
-      document;
-      img2.setAttribute('src', './images/dice2.png');
-      break;
-    case 3:
-      document;
-      img2.setAttribute('src', './images/dice3.png');
-      break;
-    case 4:
-      document;
-      img2.setAttribute('src', './images/dice4.png');
-      break;
-    case 5:
-      document;
-      img2.setAttribute('src', './images/dice5.png');
-      break;
-    case 6:
-      document;
-      img2.setAttribute('src', './images/dice6.png');
-      break;
-  }
+  const imgSource1 = './images/dice' + randomNumber1 + '.png';
+  const imgSource2 = './images/dice' + randomNumber2 + '.png';
+  img1.setAttribute('src', imgSource1);
+  img2.setAttribute('src', imgSource2);
 
   if (randomNumber1 === randomNumber2) {
     document.querySelector('h1').innerText = 'Draw!';
